a442fdca30aa4d46aa0395cf1aa5028f2fdad076
weighne <cvickery@posteo.net>
2022-02-13 08:57:03 -0500

    Cleanup

--- dice.py
+++ dice.py
@@ -8,16 +8,18 @@ map = [['A,5','B,2','A,2'],
        ['B,2','A,3','B,2']]
 
 def neighbor_check(a_coord, b_coord):
+    print(int(b_coord.split(',')[0])+pm.all())
     if a_coord == b_coord:
         print("Identical Coords!")
     else:
         if int(a_coord.split(',')[0]) == int(b_coord.split(',')[0])+pm.all() or int(a_coord.split(',')[0]) == int(b_coord.split(',')[0]):
+            print("x okay")
             if int(a_coord.split(',')[1]) == int(b_coord.split(',')[1])+pm.all() or int(a_coord.split(',')[1]) == int(b_coord.split(',')[1]):
-                print("Neighbors!")
+                return True
             else:
-                print("Not Neighbors!")
+                return False
         else:
-            print("Not Neighbors")
+            return False
 
 
 def dice_roll(num):
@@ -70,7 +72,7 @@ def update_board(attack, defend, attacker, defender):
 
 
 if __name__ == '__main__':
-    valid == True
+    valid = True
     while valid == True:
         for x in map:
             print(x)
@@ -79,11 +81,10 @@ if __name__ == '__main__':
         defender = input('Choose defender coords (x, y): ')
         attack_char, attack_stat = get_stats(attacker)
         defend_char, defend_stat = get_stats(defender)
-        if attack_char == defend_char:
+        neighbors = neighbor_check(attacker, defender)
+        print(neighbors)
+        if neighbors == True:
+            attack, defend = fight(attack_stat, defend_stat)
+            update_board(attack, defend, attacker, defender)
+        else:
             continue
-        try:
-            if attacker.split(',')[0] == defender.split(',')[0]-1 or attacker.split(',')[0] == defender.split(',')[0]+1
-
-        attack, defend = fight(attack_stat, defend_stat)
-
-        update_board(attack, defend, attacker, defender)
